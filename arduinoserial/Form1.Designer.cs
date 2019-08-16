ecb7820519d1495b782311ba312d005b0cde1ee9
Fahrul Rizal <fahrultech@gmail.com>
2019-08-16 09:55:47 +0700

    init commit

---
+++ arduinoserial/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace arduinoserial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.suhu = new System.Windows.Forms.Label();
+            this.portConfig = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboParity = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboStopbits = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboDatabits = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBaudrate = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(12, 144);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Suhu";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(633, 218);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.Chart1_Click);
+            // 
+            // suhu
+            // 
+            this.suhu.AutoSize = true;
+            this.suhu.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.suhu.Location = new System.Drawing.Point(506, 18);
+            this.suhu.Name = "suhu";
+            this.suhu.Size = new System.Drawing.Size(89, 39);
+            this.suhu.TabIndex = 2;
+            this.suhu.Text = "Data";
+            // 
+            // portConfig
+            // 
+            this.portConfig.FormattingEnabled = true;
+            this.portConfig.Location = new System.Drawing.Point(79, 23);
+            this.portConfig.Name = "portConfig";
+            this.portConfig.Size = new System.Drawing.Size(93, 21);
+            this.portConfig.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.comboParity);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.comboStopbits);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboDatabits);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBaudrate);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.portConfig);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(402, 115);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Serial Port Config";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(278, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Disconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(189, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Parity";
+            // 
+            // comboParity
+            // 
+            this.comboParity.FormattingEnabled = true;
+            this.comboParity.Items.AddRange(new object[] {
+            "None",
+            "Odd",
+            "Even",
+            "Mark",
+            "Space"});
+            this.comboParity.Location = new System.Drawing.Point(260, 50);
+            this.comboParity.Name = "comboParity";
+            this.comboParity.Size = new System.Drawing.Size(93, 21);
+            this.comboParity.TabIndex = 11;
+            this.comboParity.Text = "None";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(189, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Stop bits";
+            // 
+            // comboStopbits
+            // 
+            this.comboStopbits.FormattingEnabled = true;
+            this.comboStopbits.Items.AddRange(new object[] {
+            "One",
+            "Two"});
+            this.comboStopbits.Location = new System.Drawing.Point(260, 23);
+            this.comboStopbits.Name = "comboStopbits";
+            this.comboStopbits.Size = new System.Drawing.Size(93, 21);
+            this.comboStopbits.TabIndex = 9;
+            this.comboStopbits.Text = "One";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Data bits";
+            // 
+            // comboDatabits
+            // 
+            this.comboDatabits.FormattingEnabled = true;
+            this.comboDatabits.Items.AddRange(new object[] {
+            "8",
+            "7",
+            "6",
+            "5"});
+            this.comboDatabits.Location = new System.Drawing.Point(79, 77);
+            this.comboDatabits.Name = "comboDatabits";
+            this.comboDatabits.Size = new System.Drawing.Size(93, 21);
+            this.comboDatabits.TabIndex = 7;
+            this.comboDatabits.Text = "8";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Baudrate";
+            // 
+            // comboBaudrate
+            // 
+            this.comboBaudrate.FormattingEnabled = true;
+            this.comboBaudrate.Items.AddRange(new object[] {
+            "230400",
+            "115200",
+            "57600",
+            "38400",
+            "19200",
+            "9600",
+            "4800"});
+            this.comboBaudrate.Location = new System.Drawing.Point(79, 50);
+            this.comboBaudrate.Name = "comboBaudrate";
+            this.comboBaudrate.Size = new System.Drawing.Size(93, 21);
+            this.comboBaudrate.TabIndex = 5;
+            this.comboBaudrate.Text = "9600";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(506, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 39);
+            this.label6.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(238, 375);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(197, 39);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Status Mata";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 492);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.suhu);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Deteksi Mata";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label suhu;
+        private System.Windows.Forms.ComboBox portConfig;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboParity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboStopbits;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboDatabits;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBaudrate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ arduinoserial/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Data.SQLite;
+namespace arduinoserial
+{
+    public partial class Form1 : Form
+    {
+        private SQLiteConnection sql_con;
+        private SQLiteCommand sql_cmd;
+        private SQLiteDataAdapter DB;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = false;
+            
+        }
+
+        private void Chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            portConfig.Items.AddRange(ports);
+            chart1.ChartAreas[0].AxisY.Maximum = 400;
+            chart1.ChartAreas[0].AxisY.Minimum = 200;
+            CreateConnection();
+        }
+        private void CreateConnection()
+        {
+            sql_con = new SQLiteConnection(@"Data Source=D:\Project C# or VB\arduinoserial\arduinoserial\databaru.db;Version=3;New=False;Compress=True;");
+            try
+            {
+                sql_con.Open();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = portConfig.Text;
+                serialPort1.BaudRate = Convert.ToInt32(comboBaudrate.Text);
+                serialPort1.DataBits = Convert.ToInt32(comboDatabits.Text);
+                serialPort1.StopBits = (StopBits)Enum.Parse(typeof(StopBits), comboStopbits.Text);
+                serialPort1.Parity = (Parity)Enum.Parse(typeof(Parity), comboParity.Text);
+
+                serialPort1.Open();
+                timer1.Enabled = true;
+            }
+            catch (Exception err)
+            {
+
+                MessageBox.Show(err.Message,"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                timer1.Enabled = false;
+                serialPort1.Close();
+                label6.Text = "";
+            }
+        }
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 100;
+            String dataku = serialPort1.ReadLine().ToString();
+            label6.Text = dataku;
+            chart1.Series["Suhu"].Points.AddY(dataku);
+            SQLiteCommand sqlite_cmd;
+            sqlite_cmd = sql_con.CreateCommand();
+            sqlite_cmd.CommandText = "INSERT INTO baca (tanggal,baca) VALUES ('"+ DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "', '" + dataku + "')";
+            //sqlite_cmd.ExecuteNonQuery();
+        }
+    }
+}
